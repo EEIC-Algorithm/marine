bc26e74f724c67ec9849950a7885d673ff6e6a5d
ysugiyama12 <yuji9511yuji@gmail.com>
2020-05-14 00:45:55 +0900

    add solution.py

--- contents/07/extra-1/solution.cpp
+++ contents/07/extra-1/solution.cpp
@@ -6,6 +6,7 @@ int dp[5001][5001][2] = {};
 int a[5010];
 
 int calc(ll l, ll r, ll d){
+	cout << l << " " << r << " " << d << endl;
     int &res = dp[l][r][d];
     if(~res) return res;
     res = 1e9;
@@ -36,7 +37,7 @@ int calc(ll l, ll r, ll d){
         }
 
     }
-    return res;
+    return res; 
 
 }
 
